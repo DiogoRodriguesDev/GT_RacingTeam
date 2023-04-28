401118432102626d008261468df356d32e1ee925
Diogo Rodrigues <diogo.rodrigues@slicept.onmicrosoft.com>
2023-04-28 15:28:21 +0100

    Modificação no nome da DB

--- GT_RT_BackEnd/Data/AppDbContext.cs
+++ GT_RT_BackEnd/Data/AppDbContext.cs
@@ -1,6 +1,25 @@
-﻿namespace GT_RT_BackEnd.Data
+﻿using Microsoft.AspNetCore.Components.Server.Circuits;
+using Microsoft.EntityFrameworkCore;
+using ClassLibrary_GT_RT;
+
+namespace GT_RT_BackEnd.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext, IDataContext
     {
+        public DbSet<Carro> Carros { get; set; }
+        public DbSet<Circuito> Circuitos { get; set; }
+        public DbSet<ClassificacaoGeral> Classificacoes { get; set; }
+        public DbSet<Competicao> Competicoes { get; set; }
+        public DbSet<Corrida> Corridas { get; set; }
+        public DbSet<Equipa> Equipas { get; set; }
+        public DbSet<Piloto> Pilotos { get; set; }
+        public DbSet<Resultado> Resultados { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(@"Data Source=DIO-PC\MSSQL_SERVER_DIO;Initial Catalog=GT_RacingTeam; Trusted_Connection=True; TrustServerCertificate=True;");
+        }
     }
 }
--- GT_RT_BackEnd/Data/IDataContext.cs
+++ GT_RT_BackEnd/Data/IDataContext.cs
@@ -1,6 +1,19 @@
-﻿namespace GT_RT_BackEnd.Data
+﻿using ClassLibrary_GT_RT;
+using Microsoft.EntityFrameworkCore;
+
+namespace GT_RT_BackEnd.Data
 {
-    internal interface IDataContext
+    public interface IDataContext
     {
+        DbSet<Carro> Carros { get; set; }
+        DbSet<Circuito> Circuitos { get; set; }
+        DbSet<ClassificacaoGeral> Classificacoes { get; set; }
+        DbSet<Competicao> Competicoes { get; set; }
+        DbSet<Corrida> Corridas { get; set; }
+        DbSet<Equipa> Equipas { get; set; }
+        DbSet<Piloto> Pilotos { get; set; }
+        DbSet<Resultado> Resultados { get; set; }
+        int SaveChanges();
+        Task<int> SaveChangesAsync(CancellationToken cancellation = default);
     }
 }
