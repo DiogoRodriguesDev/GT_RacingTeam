032cb7b2fd0ccda3cf9bc7cfe3ea35f91a4ffbc6
Diogo Rodrigues <diogo.rodrigues@slicept.onmicrosoft.com>
2023-05-02 12:11:39 +0100

    Adicionada a pasta Interfaces e IWebServiceAPI

---
+++ GT_RT_FrontEnd/Interfaces/IWebServiceAPI.cs
@@ -0,0 +1,25 @@
+﻿using ClassLibrary_GT_RT;
+using Refit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GT_RT_FrontEnd.Interfaces
+{
+    public interface IWebServiceAPI
+    {
+        [Get("/pilotos")]
+        public Task<List<Piloto>> GetPilotos();
+
+        [Get("/piloto-details/{id}")]
+        public Task<Piloto> GetProduct(int id);
+
+        [Post("/pilotos")]
+        public Task<Piloto> CreateProduct([Body] Piloto newPiloto);
+
+        [Put("/piloto-edit/{id}")]
+        public Task<Piloto> UpdateProduct([FromBody] Piloto updatedPiloto, int id);
+
+
+        [Delete("/pilotos/{id}")]
+        public Task DeletePiloto(int id);
+    }
+}
