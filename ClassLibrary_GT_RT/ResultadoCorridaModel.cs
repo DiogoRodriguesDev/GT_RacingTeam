1e928fcbbc4063204c0a649c9d9cf498d0fd95d2
Diogo Rodrigues <diogo.rodrigues@slicept.onmicrosoft.com>
2023-05-11 15:24:31 +0100

    criacao de resultados a funcionar mas um bocado mal

---
+++ ClassLibrary_GT_RT/ResultadoCorridaModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_GT_RT
+{
+    public class ResultadoCorridaModel
+    {
+        public int Id_corrida { get; set; }
+        public ResultadoCorrida[] ResultadosCorrida { get; set; }
+
+    }
+}
